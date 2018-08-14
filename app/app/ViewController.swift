52a9baafda841e77d7c631cd6e26c4abbdc6073c
yagisuke <yagisuke.0209@gmail.com>
2018-08-15 00:10:11 +0900

    lesson3

--- app/app/ViewController.swift
+++ app/app/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startTimer(_ sender: Any) {
+    }
 
+    @IBAction func stopTimer(_ sender: Any) {
+    }
+
+    @IBAction func resetTimer(_ sender: Any) {
+    }
 }
-
