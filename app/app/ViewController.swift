5ff3f4b723293446d5aa3acdc0a54de587d87aa7
yagisuke <yagisuke.0209@gmail.com>
2018-08-18 15:51:04 +0900

    lesson10

--- app/app/ViewController.swift
+++ app/app/ViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
-    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+
     var startTime: TimeInterval? = nil
     var timer = Timer()
+    var elapsedTime: Double = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // start: true, stop: false, reset: false
+        setButtonEnabled(start: true, stop: false, reset: false)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,9 +31,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setButtonEnabled(start: Bool, stop: Bool, reset: Bool) {
+        self.startButton.isEnabled = start
+        self.stopButton.isEnabled = stop
+        self.resetButton.isEnabled = reset
+    }
+    
     @objc func update() {
         if let startTime = self.startTime {
-            let t: Double = Date.timeIntervalSinceReferenceDate - startTime
+            let t: Double = Date.timeIntervalSinceReferenceDate - startTime + self.elapsedTime
             let min = Int(t / 60)
             let sec = Int(t) % 60
             let msec = Int((t - Double(sec)) * 100.0)
@@ -36,6 +48,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startTimer(_ sender: Any) {
+        // start: false, stop: true, reset: false
+        setButtonEnabled(start: false, stop: true, reset: false)
+
         self.startTime = Date.timeIntervalSinceReferenceDate
         self.timer = Timer.scheduledTimer(
             timeInterval: 0.01,
@@ -46,11 +61,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func stopTimer(_ sender: Any) {
+        // start: true, stop: false, reset: true
+        setButtonEnabled(start: true, stop: false, reset: true)
+
+        if let startTime = self.startTime {
+            self.elapsedTime += Date.timeIntervalSinceReferenceDate - startTime
+        }
         self.timer.invalidate()
     }
 
     @IBAction func resetTimer(_ sender: Any) {
+        // start: true, stop: false, reset: false
+        setButtonEnabled(start: true, stop: false, reset: false)
+
         self.startTime = nil
         self.timerLabel.text = "00:00:00"
+        self.elapsedTime = 0.0
     }
 }
